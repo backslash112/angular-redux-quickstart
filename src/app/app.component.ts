b7e95493c4953af72bc37ece5d5c14b29914657a
backslash112 <yangcun@live.com>
2018-06-19 09:52:21 -0500

    use the reducer! nice!

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
+import { NgRedux } from '@angular-redux/store';
+import { IAppState } from './store';
+import { CounterActions } from './app.actions';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnDestroy {
   title = 'app';
   count: number; // <- New
+  subcription;
 
-  increment() {} // <- New
-  decrement() {} // <- New
+  constructor(private ngRedux: NgRedux<IAppState>, private actions: CounterActions) {
+    this.subcription = ngRedux.select<number>('count').subscribe(newCount => this.count = newCount);
+  }
+
+  increment() {
+    this.ngRedux.dispatch(this.actions.increment());
+  } // <- New
+  decrement() {
+    this.ngRedux.dispatch(this.actions.decrement());
+  } // <- New
+
+  ngOnDestroy() {
+
+  }
 }
