b8e86a1618d088e80d1a5087df6d53fa38083952
backslash112 <yangcun@live.com>
2018-06-19 09:58:40 -0500

    let Angular to manage the subscription for us

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,19 +2,22 @@ import { Component, OnDestroy } from '@angular/core';
 import { NgRedux } from '@angular-redux/store';
 import { IAppState } from './store';
 import { CounterActions } from './app.actions';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnDestroy {
+export class AppComponent { // implements OnDestroy
   title = 'app';
-  count: number; // <- New
-  subcription;
+  // count: number; // <- New
+  // subcription;
+  readonly count$: Observable<number>;
 
   constructor(private ngRedux: NgRedux<IAppState>, private actions: CounterActions) {
-    this.subcription = ngRedux.select<number>('count').subscribe(newCount => this.count = newCount);
+    // this.subcription = ngRedux.select<number>('count').subscribe(newCount => this.count = newCount);
+    this.count$ = ngRedux.select<number>('count');
   }
 
   increment() {
@@ -24,7 +27,7 @@ export class AppComponent implements OnDestroy {
     this.ngRedux.dispatch(this.actions.decrement());
   } // <- New
 
-  ngOnDestroy() {
-
-  }
+  // ngOnDestroy() {
+    // this.subscription.unsubscribe()
+  // }
 }
